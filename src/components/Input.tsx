b013f3df706acc2d7311cb8c9f11edb2b12bc6db
Danrlei <danrleidalfre@gmail.com>
2024-09-03 19:20:40 -0300

    add profile page

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,22 +1,27 @@
-import { ComponentProps } from 'react'
 import { Input as GluestackInput, InputField } from '@gluestack-ui/themed'
+import { ComponentProps } from 'react'
 
-type Props = ComponentProps<typeof InputField>
+type Props = ComponentProps<typeof InputField> & {
+  isReadOnly?: boolean
+}
 
-export function Input({...rest}: Props) {
+export function Input({ isReadOnly = false, ...rest }: Props) {
   return (
     <GluestackInput
-      bg='$gray700'
       h='$14'
-      px='$4'
-      borderWidth='$0'
+      borderWidth='$1'
+      borderColor='transparent'
       borderRadius='$md'
       $focus={{
         borderWidth: 1,
         borderColor: '$green500'
       }}
+      isReadOnly={isReadOnly}
+      isDisabled={isReadOnly}
     >
       <InputField
+        bg='$gray700'
+        px='$4'
         color='$white'
         fontFamily='$body'
         placeholderTextColor='$gray300'
