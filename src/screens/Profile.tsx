ed05a1ccca6f1deeef129c07b4f072d570776a9d
Danrlei <danrleidalfre@gmail.com>
2024-09-03 21:40:36 -0300

    add select photo gallery

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -3,20 +3,51 @@ import { Input } from "@components/Input";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
 import { Center, Heading, ScrollView, Text, VStack } from "@gluestack-ui/themed";
-import { TouchableOpacity } from "react-native";
+import * as FileSystem from "expo-file-system";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
+import { Alert, TouchableOpacity } from "react-native";
 
 export function Profile() {
+  const [photo, setPhoto] = useState<string>('https://github.com/danrleidalfre.png')
+
+  async function handleUserPhotoSelect() {
+    try {
+      const { canceled, assets } = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      })
+
+      if (canceled) {
+        return
+      }
+
+      const uriPhoto = assets[0].uri
+      const { size } = await FileSystem.getInfoAsync(uriPhoto) as { size: number }
+
+      if (size / 1024 / 1024 > 5) {
+        return Alert.alert('A imagem deve ser no máximo de 5MB.')
+      }
+
+      setPhoto(uriPhoto)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt="$6" px="$10">
           <UserPhoto
-            source={{ uri: 'https://github.com/danrleidalfre.png' }}
+            source={{ uri: photo }}
             alt="User Photo"
             size="xl"
           />
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               color="$green500"
               fontFamily="$heading"
